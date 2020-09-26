8dbb35d15c3d501f8d70f2c4e19805db9c5cdc47
david <kapatsadeivid@gmail.com>
2020-09-26 11:43:13 +0300

    Initial Commit

---
+++ game_of_life_console/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  game_of_life_console
+//
+//  Created by David Kapatsa on 26.09.2020.
+//  Copyright © 2020 David Kapatsa. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
